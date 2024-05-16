6e36e7f099717cd5ad3646956fc8e1d5e4fcfdc7
kakzw <kento.akazawa06@gmail.com>
2024-05-15 21:58:57 -0400

    fixed spaces

--- CyclicErrorCorrection/CyclicErrorCorrectionApp.swift
+++ CyclicErrorCorrection/CyclicErrorCorrectionApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct Cyclic_Error_CorrectionApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
